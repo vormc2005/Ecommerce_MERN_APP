b916af3c161a02c9a718cc305d3cf2c08d8cc175
vormc2005 <voronov.dmitry@gmail.com>
2020-08-25 19:45:49 -0400

    categories andproducts crud is finished, sorting is next

--- routes/category.js
+++ routes/category.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router()
 
 const{ 
-    create
+    categoryById,
+    create, 
+    read, 
+    remove, 
+    update, 
+    list
+
        } = require( "../controllers/category")
 const { requireSignin,
         isAuth,
@@ -11,12 +17,33 @@ const { requireSignin,
 const{ userById } = require( "../controllers/user");
 
 
+//GET single categry
+router.get('/category/:categoryId', read)
+//DELETE
+router.delete("/category/:categoryId/:userId",
+ requireSignin,
+    isAuth,
+    isAdmin, 
+    remove);
+//UPDATE
+router.put("/category/:categoryId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin, 
+    update);
+//CREATE CATEGORY
 router.post("/category/create/:userId",
  requireSignin,
     isAuth,
     isAdmin, 
     create);
 
+//GET ALL CATEGORIES
+router.get('/categories', list)
+
+
+
 router.param('userId', userById)
+router.param('categoryId', categoryById)
 
 module.exports = router;
